685eb8200b2e487b750dc7532cf356d7f8c644bb
Alek Festekjian <festekj2@illinois.edu>
2015-03-15 21:06:40 -0500

    First Commit

--- CalculatorInSwift/ViewController.swift
+++ CalculatorInSwift/ViewController.swift
@@ -8,18 +8,94 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    /*
+    if let displayText = display.text {
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.locale = NSLocale(localeIdentifier: "en_US")
+        if let displayNumber = numberFormatter.numberFromString(displayText) {
+            return displayNumber.doubleValue
+        }
+    }*/
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            if digit != "." || nil == display.text?.rangeOfString("."){
+                display.text = display.text! + digit
+            }
+        }
+        else{
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            enter()
+        }
+        switch operation{
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "-": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0)}
+            case "sin": performOperation { sin($0)}
+            case "cos": performOperation { cos($0)}
+            case "π": performOperation { $0 * M_PI }
+            default : break
+        }
     }
-
-
+    
+    
+   
+    
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    func performOperation(operation: (Double) -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    @IBAction func ClearAll(sender: UIButton) {
+        if(!operandStack.isEmpty){
+            operandStack.removeAll();
+        }
+        displayValue = 0
+        println("operandStack= \(operandStack)")
+    }
+    
+    
+    var operandStack =  Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        
+        operandStack.append(displayValue)
+        println("operandStack= \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+           display.text = "\(newValue)"
+           userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
